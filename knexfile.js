31a847ddb93276c3c450bae0a5e75fdf48c821f8
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-06-18 12:01:29 +0700

    Added migration files to create tables

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,7 @@ const path = require('path');
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
+console.log(DATABASE_URL);
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
