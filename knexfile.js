1ecda6c246397b3b150950ba959cbe81fae4aaf8
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-06-18 14:30:13 +0700

    Added seeds

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,10 @@ module.exports = {
     connection: DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations")
-    }
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
 };
--- src/db/migrations/20240618050052_productsAddPriceAndChangeProductNameToProductTitle.js
+++ src/db/migrations/20240618050052_productsAddPriceAndChangeProductNameToProductTitle.js
@@ -3,7 +3,10 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.table("products", (table) => {
+        table.renameColumn("product_name", "product_title");
+        table.decimal("product_price");  // Add a new column
+      });
 };
 
 /**
@@ -11,5 +14,8 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.table("products", (table) => {
+        table.renameColumn("product_title", "product_name");
+        table.dropColumn("product_price");
+      });
 };
