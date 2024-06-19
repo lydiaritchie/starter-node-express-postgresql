1da81e9064c68aa766715e0dcbe91603fb2eb9eb
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-06-19 20:45:19 +0700

    Added service.js files for products, suppliers, and categories.

--- src/suppliers/suppliers.controller.js
+++ src/suppliers/suppliers.controller.js
@@ -1,3 +1,5 @@
+const suppliersService = require("./suppliers.service.js");
+
 async function create(req, res, next) {
   res.status(201).json({ data: { supplier_name: "new supplier" } });
 }
@@ -8,6 +10,35 @@ async function update(req, res, next) {
 
 async function destroy(req, res, next) {
   res.sendStatus(204);
+}
+
+const VALID_PROPERTIES = [
+  "supplier_name",
+  "supplier_address_line_1",
+  "supplier_address_line_2",
+  "supplier_city",
+  "supplier_state",
+  "supplier_zip",
+  "supplier_phone",
+  "supplier_email",
+  "supplier_notes",
+  "supplier_type_of_goods",
+];
+
+function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  const invalidFields = Object.keys(data).filter(
+    (field) => !VALID_PROPERTIES.includes(field)
+  );
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  next();
 }
 
 module.exports = {
