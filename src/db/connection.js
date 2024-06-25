576fa5e3eae96e04a1c801564023c08e744f1ea5
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-06-25 20:09:39 +0700

    Changed env in connection.js to development

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,7 @@
-const env = process.env.NODE_ENV || "development";
+const env = "development";
 const config = require("../../knexfile")[env];
 const knex = require("knex")(config);
+
+console.log(config.client);
 
 module.exports = knex;
